efa839998c26245d1479cac7cc2578f0945eadb7
01AbdulQadeer <aqkhanodhano@gmail.com>
2025-02-11 23:34:31 +0500

    Add files via upload

---
+++ +ve or -ve num.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+   int a;
+ cout <<"check given numbers is positive or negative:";
+ cin>>a;
+ if(a>0){
+ cout<<"positive";
+ }
+ else if(a<0){
+ cout<<"negative";}
+ else
+ {
+ cout<<"invalid num";
+ }
+
+    return 0;
+ }
