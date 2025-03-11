19675e3ac6473f0cf3a5a6dd1fbea3f8ee37d0c9
01AbdulQadeer <aqkhanodhano@gmail.com>
2025-03-11 22:53:22 +0500

    Add files via upload

---
+++ Cpp_Programs/again op.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+	int a,b,op;
+	cout<<"Enter the first nnumber ";
+	cin>>a;
+	cout<<"addition+,subtraction-,multiplication*,division/ \n";
+	cin>>op;
+	cout<<"Enter the second number \n";
+	cin>>b;
+	if(op=='+')
+	{
+		cout<<"total :"<<a+b;
+	else if(op=='-')	
+	}
+}
